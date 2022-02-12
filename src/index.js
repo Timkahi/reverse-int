e47b28411b3643825a54252e179bdf6a1cb427e2
Timkahi <91555197+Timkahi@users.noreply.github.com>
2022-02-12 17:43:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function reverse (n) {
-  const stric = String(n)
-  const array = stric.split("").reverse().join("");
-  return array
+   const stric = String(n)
+   const array = stric.split("").reverse().join("");
+   return parseInt(array)
 }
